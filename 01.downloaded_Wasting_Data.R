2142c71190b7272d44c30411046d0e5005417814
Default Seadas User <seadas-user@localhost>
2021-02-15 11:51:16 -0500

    added title?

--- 01.downloaded_Wasting_Data.R
+++ 01.downloaded_Wasting_Data.R
@@ -3,4 +3,6 @@ library(readr)
 data <- read_csv("https://raw.githubusercontent.com/daviessw/BI586/master/R_Worksheet/Wasting_Data.csv", skip=1)
 library(ggplot2)
 boxplot(data$`Average WD (%)`~Transect,data=data, main="not sure what to put here",
-        xlab="Transect", ylab="Average wasting disease %")
+        xlab="Transect", ylab="Average wasting disease %", ggtitle("title"))
+
+
